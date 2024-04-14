b99b3734966c2b7ba1b7b27aabb5c3c136c57075
Blinov Ilya <ulkhan.2014@mail.ru>
2024-04-14 12:36:13 +0300

    Add FileReader

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,4 @@
 #include "utilities.h"
-#include <cmath>
 
 namespace large_prime_numbers
 {
@@ -8,7 +7,7 @@ bool are_coprime(const mpz_class& a, const mpz_class& b)
 {
     mpz_class gcd;
     mpz_gcd(gcd.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t());
-    return !(gcd > 1);
+    return gcd <= 1;
 }
 
 TwoPowersDecomposition decompose_in_powers_of_two(const mpz_class& a)
@@ -19,13 +18,5 @@ TwoPowersDecomposition decompose_in_powers_of_two(const mpz_class& a)
     mpz_fdiv_q_2exp((decomposition.odd_multiplier).get_mpz_t(), a.get_mpz_t(), decomposition.power_of_two);
     return decomposition;
 }
-
-// Custom implementation of log function for mpz_class. Inspired by gmp R package, based on mpz_get_d_2exp,
-// number = multiplier * 2 ^ exponent  ==> log(a) = log(multiplier) + exponent * log(2)
-//double mpz_log(const mpz_class& number) {
-//    signed long int exponent;
-//    const double multiplier = mpz_get_d_2exp(&exponent, number.get_mpz_t());
-//    return std::log(multiplier) + std::log(2) * static_cast<double>(exponent);
-//}
-
+s
 } // namespace large_prime_numbers
--- src/utilities.h
+++ src/utilities.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#include <gmpxx.h>
 #include <cmath>
+#include <string>
+#include <fstream>
+#include <cassert>
+#include <gmpxx.h>
 
 namespace large_prime_numbers
 {
 
 // members of decomposition: $2^power_of_two \cdot odd_multiplier$
-struct TwoPowersDecomposition {
+struct TwoPowersDecomposition
+{
     mpz_class odd_multiplier;
     uint64_t power_of_two;
 };
@@ -16,14 +20,52 @@ bool are_coprime(const mpz_class& a, const mpz_class& b);
 
 TwoPowersDecomposition decompose_in_powers_of_two(const mpz_class& a);
 
-inline double mpz_log(const mpz_class& number) {
+// Custom implementation of log function for mpz_class. Inspired by gmp R package, based on mpz_get_d_2exp,
+// number = multiplier * 2 ^ exponent  ==> log(a) = log(multiplier) + exponent * log(2)
+inline double mpz_log(const mpz_class& number)
+{
     signed long int exponent;
     const double multiplier = mpz_get_d_2exp(&exponent, number.get_mpz_t());
     return std::log(multiplier) + std::log(2) * static_cast<double>(exponent);
 }
 
-inline void mod(mpz_class& number, const mpz_class& mod_base) {
+inline void mod(mpz_class& number, const mpz_class& mod_base)
+{
     mpz_mod(number.get_mpz_t(), number.get_mpz_t(), mod_base.get_mpz_t());
 }
+
+class FileReader
+{
+ public:
+  explicit FileReader(const std::string& path_to_file)
+  {
+    file.open(path_to_file, std::ifstream::in);
+  }
+
+  bool IsOpen() {
+    return file.is_open();
+  }
+
+  template<typename T>
+  friend void operator>>(FileReader& file_reader, std::vector<T>& vec)
+  {
+    size_t numbers_amount_;
+    file_reader.file >> numbers_amount_;
+    vec.resize(numbers_amount_);
+    for (size_t i = 0; i < numbers_amount_; ++i) {
+      file_reader.file >> vec[i];
+    }
+    file_reader.file.close();
+  }
+
+  ~FileReader() {
+    if (file.is_open()) {
+      file.close();
+    }
+  }
+
+  private:
+    std::ifstream file;
+};
 
 } // namespace large_prime_numbers
